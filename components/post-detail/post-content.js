1122a683d69265d86463b5cfa2803e9781071a3c
Rostyslav Miniukov <thresh1337@gmail.com>
2021-05-03 20:09:09 +0300

    Rendering markdown with react

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -1,15 +1,67 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 
 import styles from "./post-content.module.css";
 
 function PostContent(props) {
-  const { title, image, content } = props.post;
+  const { title, image, content, slug } = props.post;
+
+  const customComponents = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === `img`) {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+
+      const language = className.split("-")[1];
+
+      return (
+        <SyntaxHighlighter
+          language={language}
+          children={children}
+          style={atomDark}
+        />
+      );
+    },
+  };
+
   return (
     <article className={styles.content}>
-      <PostHeader title={title} image={`/images/posts/${image}`} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <PostHeader title={title} image={`/images/posts/${slug}/${image}`} />
+      <ReactMarkdown components={customComponents}>{content}</ReactMarkdown>
     </article>
   );
 }
